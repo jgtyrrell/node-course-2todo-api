18a215ad16c4b2caade19db660a8f1cddfa1d35d
James Tyrrell <james@floatdesign.net>
2018-09-21 16:41:30 +0100

    Added GET /todos route

--- server/server.js
+++ server/server.js
@@ -8,12 +8,22 @@ const {Todo} = require('./models/todo');
 const app = express();
 app.use(bodyParser.json());
 
+// Creates a new todo.
 app.post('/todos', (req, res) => {
 	let todo = new Todo({
 		text: req.body.text
 	});
 	todo.save().then((doc) =>{
 		res.send(doc);
+	}).catch((err) => {
+		res.status(400).send(err);
+	});
+});
+
+// Retrieves a list of all todos.
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos});
 	}).catch((err) => {
 		res.status(400).send(err);
 	});
