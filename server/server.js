1f688e5eed6d771bc55f028f7daff61c2c702c90
James Tyrrell <james@floatdesign.net>
2018-09-23 18:43:18 +0100

    Added PATCH /todos/:id route.

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -57,6 +58,30 @@ app.delete('/todos/:id', (req, res) => {
 	}
 
 	Todo.findByIdAndDelete(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((err) => res.status(400).send());
+});
+
+// Updates a todo by ID
+app.patch('/todos/:id', (req, res) => {
+	const id = req.params.id;
+	const body = _.pick(req.body, ['text', 'completed']);
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
 		if (!todo) {
 			return res.status(404).send();
 		}
