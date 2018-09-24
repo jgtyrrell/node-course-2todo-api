dddb1fd0ac4877e52ef177492a57f8651ba953d0
James Tyrrell <james@floatdesign.net>
2018-09-24 10:38:33 +0100

    Setup separate dev and test environments.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // Tell mongoose to use native promises.
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 
 module.exports = {mongoose};
